85dce036bff8d07744600ceda02bfb19f9d1a3a4
Alannahmaxen <74313583+Alannahmaxen@users.noreply.github.com>
2021-02-12 22:26:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+canvas = document.getElemnentById('myCanvas');
+ctx = canvas.getContext("2d");
+
+car1_width = 120;
+car1_height = 70;
+car1_image = "car1.png";
+car1_x = 10;
+car1_y = 10;
+
+car2_width = 120;
+car2_height = 70;
+car2_image = "car2.png";
+car2_x = 10;
+car2_y = 10;
+
+background_image = "racing.jpg"
